39e543a53f5cacc3080eee978b493b5c7e103b76
ejonassen <jonassen.eric@gmail.com>
2014-09-19 15:02:41 -0400

    Adding portfolio page and single image

---
+++ ejportfolio.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Hello World</title>
+        <meta charset='utf-8'>
+        <link rel="stylesheet" href="portfolio.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="name">Eric Jonassen | Dynamic Web Apps Fall 2014</div>
+            <img class="pic" src="images/eric.jpg">
+        </div>
+
+        
+        <div class="wrap2">
+            <div class=head>About Me</div>
+            <p>Hi! I'm Eric Jonassen, a student in Dynamic Web Applications at Harvard Extension
+            School for the Fall 2014 Semester. I am a full time student, interested in pursuing
+            a degree in Computer Science.</p>
+            <p>I am taking this course to learn more about the field and practice of web design,
+            an area I've only recently started dabbling in. I'm interested to see if this is a field
+            that I would like to pursue further.</p>
+            <p>I have previous experience with java, javascript/jQuery, html/css, and am
+            currently taking courses in C and XML as well.</p>
+        </div>
+        <br/>
+        <div class="head">Class Projects:</div>
+        <div class="projects">
+            <div class="entry">
+                <div class="projname">Project Two</div>
+                <div><span class = "links">Link</span> | <span class="links">Code(Github)</span></div>
+            </div>
+            <br/>
+            <div class="entry">
+                <div class="projname">Project Three</div>
+                <div><span class = "links">Link</span> | <span class="links">Code(Github)</span></div>
+            </div>
+            <br/>
+            <div class="entry">
+                <div class="projname">Project Four</div>
+                <div><span class = "links">Link</span> | <span class="links">Code(Github)</span></div>
+            </div>
+        </div>
+    </body>
+</html>
